1a3a455b3c3d6e0600f79cc43e887e88b9722806
Mohamed Firas Aguel <firas.aguel@gmail.com>
2026-02-22 17:01:45 +0100

    pass stage 2

--- src/main.c
+++ src/main.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <readline/readline.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   // Flush after every printf
   setbuf(stdout, NULL);
 
   printf("$ ");
+
+  char *line = readline(NULL);
+
+  printf("%s: command not found\n", line);
+
+  free(line);
 
   return 0;
 }
